6e05aaa123d8c176cf48e4abf9bf1eedd25bc887
http://yuml.me/diagram/scruffy/class/draw <pacco.inter@gmail.com>
2017-10-21 11:33:36 +0200

    Update GameManager.cs

--- GameManager.cs
+++ GameManager.cs
@@ -1,8 +1,10 @@
 public class GameManager {
   private readonly Game _game;
+  private readonly Settings _settings;
   
-  public GameManager(Game game){
+  public GameManager(Game game, Settings settings){
     _game = game;
+    _settings = settings;
   }
   
 }
