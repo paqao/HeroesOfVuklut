47a504c4cd5a79341a1c7b73d0c7179842f43542
http://yuml.me/diagram/scruffy/class/draw <pacco.inter@gmail.com>
2017-10-21 11:36:42 +0200

    Update Game.cs

--- Game.cs
+++ Game.cs
@@ -1,3 +1,13 @@
 public class Game {
+  public Castle Castle { get; set; }
+  public IList<Hero> Heroes { get; }
   
+  public Game(){
+   Heroes = new List<Hero>(); 
+  }
+  
+  public Clear() {
+    Castle = new Castle(); 
+    Heroes.clear();
+  }
 }
