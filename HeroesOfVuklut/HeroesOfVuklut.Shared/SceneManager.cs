1aa52a4d8f73888e4995ea5546e24acfdb62f829
http://yuml.me/diagram/scruffy/class/draw <pacco.inter@gmail.com>
2017-10-22 13:34:20 +0200

    Update SceneManager.cs

--- HeroesOfVuklut/HeroesOfVuklut.Shared/SceneManager.cs
+++ HeroesOfVuklut/HeroesOfVuklut.Shared/SceneManager.cs
@@ -1,4 +1,8 @@
 public abstract class SceneManager {
   virtual void Update(decimal step){
   }
+  
+  virtual void BeginScene<T>(T sceneParameter) where T : SceneManager<typeof(this)> {
+    
+  }
 }
