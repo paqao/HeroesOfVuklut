ca7106155fa943526c47cc7c65efb4f280c5b10a
http://yuml.me/diagram/scruffy/class/draw <pacco.inter@gmail.com>
2017-10-22 13:50:08 +0200

    Update ClashMap.cs

--- HeroesOfVuklut/HeroesOfVuklut.Shared/Clash/ClashMap.cs
+++ HeroesOfVuklut/HeroesOfVuklut.Shared/Clash/ClashMap.cs
@@ -1,3 +1,13 @@
 public class ClashMap {
-
+  public ClashTile[][] Tiles { get; private set; }
+  
+  public ClashMap(int w, int h){
+    Tiles = new ClashTile[h][];
+    for(int i=0;i<h;i++){
+      Tiles[i] = new ClashTile[w];
+      for(int j=0;j<w;j++){
+        Tiles[i][j] = new ClashTile(i,j);
+      }
+    }
+  }
 }
