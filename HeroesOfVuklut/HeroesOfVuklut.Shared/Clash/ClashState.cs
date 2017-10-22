2181227b015703ae4fba92d304f539eb4f21fb30
http://yuml.me/diagram/scruffy/class/draw <pacco.inter@gmail.com>
2017-10-22 13:43:35 +0200

    Update ClashState.cs

--- HeroesOfVuklut/HeroesOfVuklut.Shared/Clash/ClashState.cs
+++ HeroesOfVuklut/HeroesOfVuklut.Shared/Clash/ClashState.cs
@@ -2,8 +2,10 @@ using System.Collections.Generic;
 
 public class ClashState {
   public IList<ClashFaction> Factions { get; protected set; }
+  public ClashMap MapClash { get; protected set; }
   
   public ClashState(){
     Factions = new List<ClashFaction>();
+    MapClash = new ClashMap();
   }
 }
