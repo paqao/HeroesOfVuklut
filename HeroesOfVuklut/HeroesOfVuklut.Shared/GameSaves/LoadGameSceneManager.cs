cca2442ba8e665bb208961a5d287e38ce8881b0c
Paweł Łabno <pacco.inter@gmail.com>
2017-12-09 14:26:08 +0100

    load scene manager

---
+++ HeroesOfVuklut/HeroesOfVuklut.Shared/GameSaves/LoadGameSceneManager.cs
@@ -0,0 +1,27 @@
+﻿using HeroesOfVuklut.Engine.DI;
+using HeroesOfVuklut.Engine.IO;
+using HeroesOfVuklut.Engine.Scenes;
+using System;
+
+namespace HeroesOfVuklut.Shared.GameSaves
+{
+
+    [SceneInject]
+    public class LoadGameSceneManager : SceneManager<LoadGameSceneManager>
+    {
+        public LoadGameSceneManager(ISceneNavigator sceneNavigator, IInputInterface inputInterface, IGraphicsInterface graphicsInterface, IGraphicElementFactory graphicElementFactory) : base(sceneNavigator, inputInterface, graphicsInterface, graphicElementFactory)
+        {
+
+        }
+
+        public override Type GetSceneType()
+        {
+            return typeof(LoadGameSceneManager);
+        }
+
+        public override void ProcessInput()
+        {
+
+        }
+    }
+}
